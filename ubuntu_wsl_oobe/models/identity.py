42e7809069c96719b8ab70a94599314f327a2936
Jinming Wu, Patrick <me@patrickwu.space>
2020-07-31 18:09:30 +0800

    hook up the user setup logic

--- ubuntu_wsl_oobe/models/identity.py
+++ ubuntu_wsl_oobe/models/identity.py
@@ -18,6 +18,7 @@ import logging
 import attr
 
 from subiquitycore.utils import crypt_password
+from subiquitycore.utils import run_command
 
 
 log = logging.getLogger('subiquity.models.identity')
@@ -39,13 +40,15 @@ class IdentityModel(object):
     def add_user(self, result):
         result = result.copy()
         self._user = User(**result)
+        run_command(["/usr/sbin/useradd", "-m", "-s", "/bin/bash", "-p", result['password'], result['username']])
+        run_command(["/usr/sbin/usermod", "-a", "-G", "adm,dialout,cdrom,floppy,sudo,audio,dip,video,plugdev,netdev", result['username']])
 
     @property
     def user(self):
         return self._user
 
     def encrypt_password(self, passinput):
-        return crypt_password(passinput)
+        return crypt_password(passinput, 'MD5')
 
     def __repr__(self):
         return "<LocalUser: {} {}>".format(self.user, self.hostname)
