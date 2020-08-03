23cf5224be9204761fd5b22bbe786ef03bdd9c30
Jinming Wu, Patrick <me@patrickwu.space>
2020-08-03 18:23:25 +0800

    Update overall model

--- ubuntu_wsl_oobe/models/ubuntu_wsl_oobe.py
+++ ubuntu_wsl_oobe/models/ubuntu_wsl_oobe.py
@@ -1,6 +1,15 @@
+import logging
+import asyncio
+
 from .identity import IdentityModel
 from .locale import LocaleModel
 
+log = logging.getLogger('subiquity.models.subiquity')
+
+ALL_MODEL_NAMES = [
+    "identity",
+    "locale"
+]
 
 class ConsoleConfModel:
     """The overall model for console-conf."""
@@ -8,3 +17,12 @@ class ConsoleConfModel:
     def __init__(self):
         self.identity = IdentityModel()
         self.locale = LocaleModel()
+
+        self._events = {
+            name: asyncio.Event() for name in ALL_MODEL_NAMES
+            }
+
+    def configured(self, model_name):
+        log.debug("model %s is configured", model_name)
+        self._events[model_name].set()
+        
